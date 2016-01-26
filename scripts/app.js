3328abd9c2825fc2216daebee5a269c0572a710e
caseyvianen <c.vianen@mondriaanict.nl>
2016-01-26 13:03:44 +0100

    First Attempt

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+angular.module("Vergaderen", [])
+.controller('mainCtrl', function  ($scope){
+  $scope.deleteVergadering = function(){
+    console.log("Hallo Wereld");
+  };
+             $scope.Vergaderingen = [];
+             $scope.deleteVergadering = function(Vergaderingen,$index)
+             {
+               $scope.Vergaderingen.splice($index,1);
+             };
+
+             $scope.addVergadering = function(n,d,i,t)
+             {
+               var Boeking={Name:n,Doel:d,Informatie:i,Tijd:t}
+                 $scope.Vergaderingen.push(Boeking);
+
+               }
+             });
