3941a0fb3e439c65f0aafc0147b59181f762f16b
caseyvianen <c.vianen@mondriaanict.nl>
2016-01-28 16:52:56 +0100

    Database gelinked aan input waardes

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,28 @@
 angular.module("Vergaderen", ['ngResource','lbServices'])
 .controller('mainCtrl', function  ($scope,Booking){
-             $scope.Vergaderingen = [];
-            $scope.deleteVergadering = function(Vergaderingen,$index)
+              $scope.bookings = Booking.find();
+            $scope.deleteVergadering = function($index)
              {
-               $scope.Vergaderingen.splice($index,1);
-             };
 
-             $scope.addVergadering = function(n,d,i,t)
+               Booking.deleteById({ id: $scope.bookings[$index].id })
+                 .$promise
+                 .then(function() { console.log('deleted'); });
+                 $scope.bookings.splice($index,1);
+
+           }
+
+             $scope.addVergadering = function(n,p,i,d)
              {
-                 var Boeking={Name:n,Doel:d,Informatie:i,Tijd:t}
-                   $scope.Vergaderingen.push(Boeking);
+               var tijd= new Date(d).toISOString();
+               console.log(d);
+                  Booking.create({
+                   "name":n,
+                   "purpose":p,
+                   "information":i,
+                   "time": tijd
+                 });
+                  $scope.bookings = Booking.find();
+             }
 
 
-
-}
-
-
-             });
+           });
